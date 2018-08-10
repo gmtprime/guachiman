84814242c64949b372dc59a87efe1eb9e0c93b6e
José San Gil <jsangilve@gmail.com>
2018-08-10 11:24:34 +0200

    add minor log entry to Guardian's error_handler

--- lib/guachiman/error_handler.ex
+++ lib/guachiman/error_handler.ex
@@ -1,5 +1,9 @@
 defmodule Guachiman.AuthErrorHandler do
+  require Logger
+
   def auth_error(conn, {type, reason}, _opts) do
+    # TODO log client and reason the authentication failed
+    Logger.info("#{type} - #{reason}")
     msg =
       case reason do
         :invalid_token -> "Invalid authentication token"
